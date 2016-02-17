023225f0640164f32e202343ffbff74704a224ef
Sebastien Dumetz <s.dumetz@holusion.com>
2016-02-17 11:12:48 +0100

    enable package listing for repos

--- lib/components/Repos.jsx
+++ lib/components/Repos.jsx
@@ -13,12 +13,27 @@ export default class Repos extends React.Component{
     });
   }
   render(){
-    var p = this.state.packages.map(function(pack){
-      return (<div>{pack}</div>)
+    var p = this.state.packages.map(function(pack,index){
+      var infos = pack.split(" "); // it'sl ike Pamd64 aptly 1.0.1 5d2eb872323a639f
+
+      return (<tr key={index}>
+        <td className="mdl-data-table__cell--non-numeric">{infos[0].slice(1)}</td>
+        <td className="mdl-data-table__cell--non-numeric">{infos[1]}</td>
+        <td className="mdl-data-table__cell--non-numeric">{infos[2]}</td>
+      </tr>)
     })
     return (<div>
       <h1>{this.props.routeParams.name}</h1>
-      <div>{p}</div>
+      <table className="mdl-data-table mdl-js-data-table mdl-shadow--2dp">
+      <thead><tr>
+        <th className="mdl-data-table__cell--non-numeric">Arch</th>
+        <th className="mdl-data-table__cell--non-numeric">Name</th>
+        <th className="mdl-data-table__cell--non-numeric">Version</th>
+      </tr></thead>
+      <tbody>
+        {p}
+      </tbody>
+      </table>
       </div>)
     }
 }
