888857ba37a84fae49603dfdcb42ac6bd41a0134
Sebastien Dumetz <s.dumetz@holusion.com>
2016-03-03 16:20:00 +0100

    update styles and fonts

--- lib/components/layout/Navbar.jsx
+++ lib/components/layout/Navbar.jsx
@@ -17,9 +17,8 @@ export default class Navbar extends React.Component{
       <div className="mdl-layout mdl-js-layout mdl-layout--fixed-header has-drawer">
       <header className="mdl-layout__header is-casting-shadow">
         <div className="mdl-layout__header-row">
-          <span className="mdl-layout-title">{this.props.title}</span>
+          <span className="docs-layout-title mdl-layout-title" style={{paddingRight:10}}><Link to="/ui" >{this.props.title}</Link></span>
           <nav className="mdl-navigation nav-path" style={{width:"100%"}}>
-            <Link to="/ui" className="mdl-navigation__link">Home</Link>
             {paths}
           </nav>
           </div>
