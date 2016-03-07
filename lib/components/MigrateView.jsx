339cf892b5dbd7250dfe0cccd453df6105cc8096
Sebastien Dumetz <s.dumetz@holusion.com>
2016-03-07 17:45:45 +0100

    update MigrateView to prototype future Views

--- lib/components/MigrateView.jsx
+++ lib/components/MigrateView.jsx
@@ -5,14 +5,48 @@ import PackageDetails from "./PackageDetails.jsx"
 export default class MigrateView extends React.Component{
   constructor(props){
     super(props)
+    this.state = {presence:[]};
   }
-  static get contextTypes(){
-    return {router: React.PropTypes.object.isRequired}
+  get infos(){
+    return parseKey(this.props.routeParams.key);
+  }
+  componentDidMount(){
+    request.getJSON("/api/repos").then((r)=>{
+      var packages = r.map((repo)=>{
+        return request.getJSON(`/api/repos/${repo.Name}/packages?q=${this.infos.name}_${this.infos.version}_${this.infos.arch}`).catch((e)=>{return []})
+      });
+      Promise.all(packages).then((p)=>{
+        var fin = r.reduce(function(repos,repo,index){
+          repos[repo.Name] = (packages[index].length >0)?true:false;
+          return repos;
+        },{});
+        console.log("setState : ",fin)
+        this.setState({presence:fin});
+      });
+    });
   }
   render(){
+    var repos = Object.keys(this.state.presence).map((repo)=>{
+      return (<tr key={repo}>
+        <td>{repo}</td>
+        <td>
+          <label className="mdl-switch mdl-js-switch mdl-js-ripple-effect" htmlFor="switch-1">
+            <input type="checkbox" id="switch-1" className="mdl-switch__input" checked={this.state.presence[repo]}/>
+            <span className="mdl-switch__label"></span>
+          </label>
+        </td>
+      </tr>)
+    })
     return (<div style={{display:"flex"}}>
       <div style={{padding:"0px 10px 25px 0px"}}>
-        {this.props.routeParams.key}
+        <table>
+        <thead>
+          <tr><th>Repo</th><th>Presence</th></tr>
+        </thead>
+        <tbody>
+          {repos}
+        </tbody>
+        </table>
       </div>
     </div>)
   }
