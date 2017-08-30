b87ae20b7e5629505a5aedfbc482e5e3b22c9c2e
Sebastien Dumetz <s.dumetz@holusion.com>
2017-08-30 10:26:32 +0200

    update deploy mechanism to use github releases

--- deploy.sh
+++ deploy.sh
@@ -19,19 +19,8 @@ mkdir -p $TEMP_DIRECTORY || exit 1
 npm run webpack || exit 1
 for file in ${FILES[@]}; do cp -r "${file}" $TEMP_DIRECTORY||exit 1; done
 
-git checkout -B gh-pages || exit 1
-echo "Removing old static content"
-rm -rf ./* .gitignore
 echo "copying new content"
 cd $BUILD_DIRECTORY && tar -zcf $DIR/aptly-web-ui.tar.gz * && cd $DIR ||exit 1
-
-echo "Pushing new content to $ORIGIN_URL"
-git config user.name "Travis CI" || exit 1
-git config user.email "nobody@nowhere.org" || exit 1
-git add aptly-web-ui.tar.gz || exit 1
-git commit -m "Rebuild for $CURRENT_COMMIT ($(date +"%a %d %b %Y %R"))" || exit 1
-echo "push url : ${ORIGIN_URL}"
-git push --force --quiet "$ORIGIN_URL_WITH_CREDENTIALS" gh-pages ||exit 1
 
 echo "Cleaning up temp files"
 rm -Rf $TEMP_DIRECTORY
