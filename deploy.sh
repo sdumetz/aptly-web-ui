49e9ddddad03117248681a811b46804063400c66
Sebastien Dumetz <s.dumetz@holusion.com>
2017-09-14 15:15:52 +0200

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,6 @@ echo "Starting deployment"
 BUILD_DIRECTORY="/tmp/tmp_build_dir"
 TEMP_DIRECTORY=$BUILD_DIRECTORY/ui
 DIR=$(pwd)
-CURRENT_COMMIT=`git rev-parse HEAD`
-ORIGIN_URL=`git config --get remote.origin.url`
-ORIGIN_URL_WITH_CREDENTIALS="${ORIGIN_URL/github.com/$GH_TOKEN@github.com}"
 
 FILES=(
   'index.html'
